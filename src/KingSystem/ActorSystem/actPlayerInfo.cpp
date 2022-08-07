1d494e02288083e2592cb10c910a9a3bd07d4338
ecumber <ecumber05@gmail.com>
2022-08-07 09:32:13 -0700

    Continue PlayerInfo

--- src/KingSystem/ActorSystem/actPlayerInfo.cpp
+++ src/KingSystem/ActorSystem/actPlayerInfo.cpp
@@ -1,1 +1,8 @@
 #include "KingSystem/ActorSystem/actPlayerInfo.h"
+
+namespace ksys::act {
+
+SEAD_SINGLETON_DISPOSER_IMPL(PlayerInfo)
+
+
+};
--- src/KingSystem/ActorSystem/actPlayerInfo.h
+++ src/KingSystem/ActorSystem/actPlayerInfo.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <heap/seadDisposer.h>
+#include <math/seadMatrix.h>
+#include "KingSystem/ActorSystem/actActorConstDataAccess.h"
+#include "KingSystem/ActorSystem/actActorLinkConstDataAccess.h"
 
 namespace ksys::act {
 
@@ -13,10 +16,15 @@ class PlayerInfo {
     virtual ~PlayerInfo();
 
 public:
+    struct S0 {}
     PlayerBase* getPlayer() const;
     s32 getLife() const;
     u32 getMaxHearts() const;
     f32 getMaxStamina() const;
+    sead::Matrix34f* getPlayerPosFromPlayerInfo();
+private:
+    
+
 };
 
 }  // namespace ksys::act
